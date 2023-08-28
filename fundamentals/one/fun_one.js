ae3d8fc00d7024bdb06c29e88b43bc7c5f11c3cc
Dalius Beck <daliusbeckxiv@gmail.com>
2023-08-27 19:21:38 -0700

    Update fun_one.js

--- fundamentals/one/fun_one.js
+++ fundamentals/one/fun_one.js
@@ -58,13 +58,14 @@
 
 // Print integers from 2000 to 5280, using a WHILE.
 
-function doWhile() {
-    var num = 2000;
+// function doWhile() {
+//     var num = 2000;
 
-    while (num <= 5280){
-        console.log(num);
-        num ++;
-    }
-}
+//     while (num <= 5280){
+//         console.log(num);
+//         num ++;
+//     }
+// }
 
-console.log(doWhile())
+// console.log(doWhile())
+
